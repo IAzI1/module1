602d6e940da99f7170bab989b423685c5cf4284f
Aziz <ibodov.aziz@yandex.ru>
2024-11-01 13:24:20 +0300

    done

---
+++ Variables.py
@@ -0,0 +1,8 @@
+completed_works = 12
+count_of_hours = 1.5
+course_name = 'Python'
+time_for_task = count_of_hours / completed_works
+
+print(
+    f'Курс: {course_name}, всего задач: {completed_works}, '
+    f'затрачено часов: {count_of_hours}, среднее время выполнения {time_for_task} часа.')
